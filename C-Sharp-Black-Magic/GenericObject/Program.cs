b23f881a61b6b21ee47f9859d87174c39958f951
Palmér <simon.palmer@telia.com>
2018-03-05 21:40:10 +0100

    Added GenericObject demo

---
+++ C-Sharp-Black-Magic/GenericObject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    /// <summary>
+    /// This code demonstrates how you can create an instantiated object with a generic value.
+    /// </summary>
+    /// <param name="args"></param>
+
+    static void Main(string[] args)
+    {
+        object obj = GetGenericObject();
+        // This evaluation will return True. You now have an instantiated object with a generic value.
+        var isObjGeneric = obj.GetType().GetTypeInfo().IsGenericTypeDefinition;
+        Console.WriteLine(isObjGeneric);
+    }
+
+    static object GetGenericObject()
+    {
+        var type = typeof(Foo<>.GenericEnum).GetTypeInfo();
+        var decField = type.GetDeclaredField("Bar");
+        var obj = decField.GetValue(null);
+        return obj;
+    }
+
+    class Foo<T>
+    {
+        public enum GenericEnum
+        {
+            Bar = 0
+        }
+    }
+}
