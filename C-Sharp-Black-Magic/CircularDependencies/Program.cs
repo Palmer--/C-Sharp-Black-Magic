a1e5b3dc12174236c8f3ebfa1e1cfac9af452137
Palmér <11057262+Palmer--@users.noreply.github.com>
2018-03-07 22:57:59 +0100

    Added CircularDependencies example

---
+++ C-Sharp-Black-Magic/CircularDependencies/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularDependencies
+{
+    class Program
+    {
+        //This code demonstrates how circular dependencies in constructors can result in wierd values.
+        //These kinds of heisenbugs can be very hard track down.
+
+        static void Main(string[] args)
+        {
+            //One would expect val to be 10 here, but we actually end up with 0 due to circular dependency.
+            int val = Foo.A;
+
+        }
+
+        static class Foo
+        {
+            public static int A = Bar.B;
+            public static int C = 10;
+        }
+
+        static class Bar
+        {
+            //Since we are getting C but the static constructor for Foo is already running we actually end up with C = 0
+            public static int B = Foo.C;
+        }
+    }
+}
